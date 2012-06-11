5a746ab6883a6002410a49e7bc0677b363d91fdd
Jill Burrows <jill@puppetlabs.com>
2012-06-11 15:25:41 -0700

    Reverting "Disallow authentication of users with a status other than enabled"
    This change is no longer needed. A user being enabled or diasbled is handled
    in the authorization layer.

--- lib/casserver/authenticators/sql_encrypted.rb
+++ lib/casserver/authenticators/sql_encrypted.rb
@@ -54,8 +54,8 @@ class CASServer::Authenticators::SQLEncrypted < CASServer::Authenticators::SQL
 
     log_connection_pool_size
     $LOG.debug "#{self.class}: [#{user_model}] " + "Connection pool size: #{user_model.connection_pool.instance_variable_get(:@checked_out).length}/#{user_model.connection_pool.instance_variable_get(:@connections).length}"
-    results = user_model.find(:all, :conditions => ["#{username_column} = ? AND status = 'enabled'", @username])
 
+    results = user_model.find(:all, :conditions => ["#{username_column} = ?", @username])
     user_model.connection_pool.checkin(user_model.connection)
 
     if results.size > 0
